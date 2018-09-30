29ea25a5b2e01681761c41c2002c3834781848ca
Gheorghe <hackscomicon@live.com>
2018-09-30 12:28:00 +0300

    added some documentation

--- EvilBot/ITwitchConnections.cs
+++ EvilBot/ITwitchConnections.cs
@@ -3,6 +3,9 @@ using TwitchLib.Client;
 
 namespace EvilBot
 {
+    /// <summary>
+    /// Only used for getting TwitchConnections from a connections class.
+    /// </summary>
     internal interface ITwitchConnections
     {
         TwitchAPI Api { get; }
