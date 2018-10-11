9598f5221f4f6a7918f213f8d449695826d536ff
Gheorghe <hackscomicon@live.com>
2018-10-11 22:44:34 +0300

    changed bot version in title

--- EvilBot/App.cs
+++ EvilBot/App.cs
@@ -17,7 +17,7 @@ namespace EvilBot
 
         private static void SetConsoleMode()
         {
-            Console.Title = @"EvilBot for Twitch by M0rtuary";
+            Console.Title = @"EvilBot v0.2.2beta for Twitch by M0rtuary";
             Console.TreatControlCAsInput = true;
         }
 
