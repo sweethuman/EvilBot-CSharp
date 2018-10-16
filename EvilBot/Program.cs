0152c948b2a9268ba07bf48664af6b04240bdd8d
Gheorghe <hackscomicon@live.com>
2018-10-16 12:20:18 +0300

    version 0.3.2 and disable 'X' menu

--- EvilBot/Program.cs
+++ EvilBot/Program.cs
@@ -1,11 +1,25 @@
-﻿using Autofac;
+﻿using System;
+using System.Runtime.InteropServices;
+using Autofac;
 
 namespace EvilBot
 {
     internal static class Program
     {
+        private const int MF_BYCOMMAND = 0x00000000;
+        public const int SC_CLOSE = 0xF060;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
         private static void Main(string[] args)
         {
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false),SC_CLOSE, MF_BYCOMMAND);
             var container = ContainerConfig.Config();
             using (var scope = container.BeginLifetimeScope())
             {
